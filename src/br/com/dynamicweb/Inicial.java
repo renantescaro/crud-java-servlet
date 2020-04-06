3b6d3d6fc84f5b2abca401970c69ee12f367f4b5
renantescaro <renantescaro@yahoo.com.br>
2020-04-06 19:32:08 -0300

    criando projeto Servlet

---
+++ src/br/com/dynamicweb/Inicial.java
@@ -0,0 +1,25 @@
+package br.com.dynamicweb;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class Inicial extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse reponses) throws IOException{
+		PrintWriter html = reponses.getWriter();
+		
+		html.println("<html>");
+		html.println("<body>");
+		html.println("<p>Bem Vindo ao Servlet</p>");
+		html.println("</body>");
+		html.println("</html>");
+		
+		
+	}
+}
